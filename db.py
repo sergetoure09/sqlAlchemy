1788b0bf7e9053b36248d4cb58d359eae6dd2cfb
sergetoure09 <sergetoure09@gmail.com>
2017-11-23 16:16:08 +0000

    insert

--- db.py
+++ db.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from app import app
+from datetime import date
 #from sqlalchemy import MetaData
 '''
 convention = {
@@ -24,4 +25,21 @@ print('creating table...')
 db.create_all()
 print('table book successfully created...')
 '''
+class Member(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    username=db.Column(db.String(30),unique=True)
+    password=db.Column(db.String(30))
+    email=db.Column(db.String(30),unique=True)
+    join_date=db.Column(db.DateTime)
+
+    def __rep__(self):
+        return '<Member %r>' % self.username
+
+db.create_all()
+today=date.today()
+
+Serge=Member(username="serge10",password='secreter',email='sergeant@gmail.com',join_date=today)
+db.session.add(Serge)
+db.session.commit()
+
 
